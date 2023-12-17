cddc6acaa3939f5d9a775abb44563e331c405246
verysluggishdev <verysluggishdev@gmail.com>
2023-12-17 10:45:05 +0300

    Added function to generate unique file names for uploads

--- gateway/routers/upload.py
+++ gateway/routers/upload.py
@@ -1,8 +1,11 @@
 from typing import Optional
 from .. import schemas,models, oauth2
-from fastapi import Depends, APIRouter, UploadFile, File
+from fastapi import Depends, APIRouter, UploadFile, File, status
 from sqlalchemy.orm import Session
 from ..database import get_db 
+from ..utils import generate_unique_file_name
+import os
+import base64
 
 
 router = APIRouter(
@@ -10,19 +13,23 @@ router = APIRouter(
     tags=["Upload"]
 )
 
-@router.post("")
+@router.post("", status_code=status.HTTP_201_CREATED)
 def upload(file: UploadFile = File(...), db: Session = Depends(get_db), current_user = Depends(oauth2.get_current_user)):
     try:
+        if not os.path.exists('../../uploads'):
+            os.mkdir('../../uploads')
         contents = file.file.read()
-        with open(file.filename, 'wb') as f:
+        filename = generate_unique_file_name(file.filename)
+        print(base64.b64decode(filename.encode()))
+        with open(f'uploads/{filename}', 'wb') as f:
             f.write(contents)
-    except Exception:
+    except Exception as e:
+        print(e)
         return {"message": "There was an error uploading the file"}
     finally:
         file.file.close()
-    new_avatar = models.Avatar(owner_id=current_user.id, filename=file.filename)
+    new_avatar = models.Avatar(owner_id=current_user.id, filename=filename)
     db.add(new_avatar)
     db.commit()
     db.refresh(new_avatar)
-
-    return {"message": f"Successfully uploaded {file.filename}"}
+    return {"message": f"Avatar was updated successfully"}
--- gateway/utils.py
+++ gateway/utils.py
@@ -1,4 +1,7 @@
 from passlib.context import CryptContext
+from datetime import datetime
+import base64
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
@@ -8,3 +11,8 @@ def hash(password: str):
 
 def verify(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+def generate_unique_file_name(filename):
+    current_time = str(datetime.now())
+    unique_file_name = f"{filename}@{current_time}"
+    return base64.b64encode(unique_file_name.encode()).decode()
