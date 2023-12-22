91b8ddd82d2798d9d3d2cb91b030a107d3b611e9
verysluggishdev <verysluggishdev@gmail.com>
2023-12-22 15:28:53 +0300

    Added more CORS middleware

--- gateway/main.py
+++ gateway/main.py
@@ -8,6 +8,14 @@ origins = ["*"]
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(posts.router)
