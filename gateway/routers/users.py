a23e7b5a4ccfe65611c1df187be6febdc99b0e23
verysluggishdev <verysluggishdev@gmail.com>
2024-01-04 15:44:32 +0300

    Added functionality to subscribe and unsubscribe to users

--- gateway/routers/users.py
+++ gateway/routers/users.py
@@ -101,3 +101,19 @@ def delete_user(id: int, db: Session = Depends(get_db), current_user: models.Use
     user_query.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@router.post("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def subscribe_to_user(id: int, current_user: int = Depends(oauth2.get_current_user), db: Session = Depends(get_db)):
+    user_query = db.query(models.User).filter(models.User.id==id)
+    user = user_query.first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"user with id: {id} was not found")    
+    current_subscription_query = db.query(models.Subscribers).filter(models.Subscribers.user_id == current_user.id).filter(models.Subscribers.subscribed_to == id)
+    has_subscribed = current_subscription_query.first()
+    if has_subscribed:
+        current_subscription_query.delete(synchronize_session=False)
+    else:
+        new_subscription = models.Subscribers(user_id=current_user.id, subscribed_to=id)
+        db.add(new_subscription)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
